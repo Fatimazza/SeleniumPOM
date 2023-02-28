ecd37c64bdb89eb2d1d0118b5b76ab1a6f417c85
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-02-28 16:50:20 +0700

    Listing Page - Add function to Get Header Title

--- PageObjects/productListingPage.py
+++ PageObjects/productListingPage.py
@@ -1,6 +1,10 @@
 from selenium.webdriver.common.by import By
 
 class ProductListingPage:
+    header_product_xpath = "//div[@id='header_container']//span[@class='title']"
 
     def __init__(self, driver):
         self.driver = driver
+
+    def getHeaderTitle(self):
+        return self.driver.find_element(By.XPATH,self.header_product_xpath).text
