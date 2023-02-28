0d7cf6a70a2ed1a3bc93ed5d302b5b668a4bc8c8
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-02-28 16:29:33 +0700

    Test Login - Add Login unit test class configured with Chrome Driver

---
+++ TestCases/test_login.py
@@ -0,0 +1,18 @@
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+class SaucedemoLogin(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        self.browser.get(self.baseUrl)
+
+    def tearDown(self):
+        self.browser.close()
+
+if __name__ == "__main__": 
+    # run all test
+    unittest.main()
