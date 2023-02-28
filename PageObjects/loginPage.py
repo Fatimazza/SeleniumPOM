2b7fbc2357084bcf3c6112dc75ba37077b0ff0ff
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-02-28 16:39:02 +0700

    Login Page - Add Set Username function

--- PageObjects/loginPage.py
+++ PageObjects/loginPage.py
@@ -1,6 +1,11 @@
 from selenium.webdriver.common.by import By
 
 class LoginPage:
+    textbox_username_id = "user-name"
 
     def __init__(self, driver):
         self.driver = driver
+
+    def setUsername(self, username):
+        self.driver.find_element(By.ID, self.textbox_username_id).clear()
+        self.driver.find_element(By.ID, self.textbox_username_id).send_keys(username)
