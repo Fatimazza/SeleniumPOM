84f9728bcbfb2ddd34ea49849277f4cc6ca83df0
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-02-28 16:42:55 +0700

    Login Page - Add Set Password function

--- PageObjects/loginPage.py
+++ PageObjects/loginPage.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 
 class LoginPage:
     textbox_username_id = "user-name"
+    textbox_password_id = "password"
 
     def __init__(self, driver):
         self.driver = driver
@@ -9,3 +10,7 @@ class LoginPage:
     def setUsername(self, username):
         self.driver.find_element(By.ID, self.textbox_username_id).clear()
         self.driver.find_element(By.ID, self.textbox_username_id).send_keys(username)
+
+    def setPassword(self, password):
+        self.driver.find_element(By.ID, self.textbox_password_id).clear()
+        self.driver.find_element(By.ID, self.textbox_password_id).send_keys(password)
