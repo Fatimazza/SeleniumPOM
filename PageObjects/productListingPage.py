05e054339ee1c79299b7d8395e6327068560dbdf
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-02-28 21:43:19 +0700

    Listing Page - Add function to Click Logout link

--- PageObjects/productListingPage.py
+++ PageObjects/productListingPage.py
@@ -4,6 +4,7 @@ class ProductListingPage:
     header_product_xpath = "//div[@id='header_container']//span[@class='title']"
     first_product_name_class = "inventory_item_name"
     button_burger_menu_id = "react-burger-menu-btn"
+    link_logout_linktext = "Logout"
 
     def __init__(self, driver):
         self.driver = driver
@@ -16,3 +17,6 @@ class ProductListingPage:
 
     def clickBurgerMenu(self):
         self.driver.find_element(By.ID, self.button_burger_menu_id).click()
+    
+    def clickLogout(self):
+        self.driver.find_element(By.LINK_TEXT, self.link_logout_linktext).click()
