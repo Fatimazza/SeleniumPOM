006046747920d4541dfaedf7f0fb134de056d40c
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-03-01 07:56:08 +0700

    Test Product Detail - Add step for Login

--- TestCases/test_product.py
+++ TestCases/test_product.py
@@ -5,12 +5,24 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
+from PageObjects.loginPage import LoginPage
+
 class SaucedemoProduct(unittest.TestCase):
     baseUrl = "https://www.saucedemo.com/"
+    username = "standard_user"
+    password = "secret_sauce"
 
     def setUp(self):
         self.browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
         self.browser.get(self.baseUrl)
+        time.sleep(5)
+
+    def test_a_success_product_open_detail(self):
+        # step to login
+        loginPage = LoginPage(self.browser)
+        loginPage.setUsername(self.username)
+        loginPage.setPassword(self.password)
+        loginPage.clickLogin()
         time.sleep(5)
 
     def tearDown(self):
