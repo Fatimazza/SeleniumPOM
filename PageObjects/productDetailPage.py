a3305090e43bf052d03cd238f4e0c4c3f1037811
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-03-01 11:06:33 +0700

    Detail Page - Add functions to get Product name, description, and price

--- PageObjects/productDetailPage.py
+++ PageObjects/productDetailPage.py
@@ -1,6 +1,18 @@
 from selenium.webdriver.common.by import By
 
 class ProductDetailPage:
+    text_product_name_css = "div.inventory_details_name"
+    text_product_desc_css = "div.inventory_details_desc"
+    text_product_price_css = "div.inventory_details_price"
 
     def __init__(self, driver):
         self.driver = driver
+
+    def getProductName(self):
+        return self.driver.find_element(By.CSS_SELECTOR, self.text_product_name_css).text
+
+    def getProductDesc(self):
+        return self.driver.find_element(By.CSS_SELECTOR, self.text_product_desc_css).text
+
+    def getProductPrice(self):
+        return self.driver.find_element(By.CSS_SELECTOR, self.text_product_price_css).text
