33bbdf3f6570b31e40878c008c9051a6c1b1d176
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-03-01 10:40:41 +0700

    Listing Page - Change to shorter Selector for third product name

--- PageObjects/productListingPage.py
+++ PageObjects/productListingPage.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 class ProductListingPage:
     text_header_xpath = "//div[@id='header_container']//span[@class='title']"
     text_first_product_name_class = "inventory_item_name"
-    text_third_product_name_xpath = "/html//div[@id='inventory_container']/div/div[@id='inventory_container']/div/div[3]/div[@class='inventory_item_description']/div[@class='inventory_item_label']/a[@href='#']/div[@class='inventory_item_name']"
+    text_third_product_name_css = ".inventory_list .inventory_item:nth-of-type(3) .inventory_item_name"
     button_burger_menu_id = "react-burger-menu-btn"
     link_logout_linktext = "Logout"
 
@@ -17,7 +17,7 @@ class ProductListingPage:
         return self.driver.find_element(By.CLASS_NAME, self.text_first_product_name_class).text
 
     def getThirdProductName(self):
-        return self.driver.find_element(By.XPATH, self.text_third_product_name_xpath).text
+        return self.driver.find_element(By.CSS_SELECTOR, self.text_third_product_name_css).text
 
     def clickBurgerMenu(self):
         self.driver.find_element(By.ID, self.button_burger_menu_id).click()
